755962de63040685c872b46a5553a706d3d25c7b
chen <331862806@qq.com>
2019-07-30 17:54:32 +0800

    栅格 空隙

--- src/col.vue
+++ src/col.vue
@@ -1,32 +1,55 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot></slot>
+    <div class="col" :class="[span && `col-${span}`,offset && `offset-${offset}`]"
+         :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+
+        <div style="border: 1px solid green; height: 100px;">
+            <slot></slot>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "col",
+        name: "GuLuCol",
         props: {
             span: {
                 type: [Number, String]
+            },
+            offset: {
+                type: [Number, String]
+            },
+            gutter: {
+                type: [Number, String]
             }
-        }
+        },
+        // data () {
+        //     return {
+        //         gutter: 0
+        //     }
+        // },
+        created() {
+            console.log("row created")
+        },
+
+        mounted() {
+            console.log("row mounted");
+        },
     }
 </script>
+<style scoped lang="scss"> .col {width: 50%;
 
-<style scoped lang="scss">
-    .col {
-        height: 100px;
-        background: gray;
-        width: 50%;
-        border: solid 1px red;
-
-        $class: col- ;
-        @for $n from 1 through 24 {
-            &.#{$class}#{$n} {
-                width: ($n/24)*100%;
-            }
+    $class: col-;
+    @for $n from 1 through 24 {
+        &.#{$class}#{$n} {
+            width: ($n/24)*100%;
         }
     }
+
+    $class: offset-;
+    @for $n from 1 through 24 {
+        &.#{$class}#{$n} {
+            margin-left: ($n/24)*100%;
+        }
+    }
+}
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,17 +1,41 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        name: "row"
+        name: "GuLuRow",
+        props: {
+            gutter: {
+                type: [Number, String]
+            }
+        },
+        created() {
+            // 没有儿子，时间差
+            // console.log(this.$children);
+            console.log("row created")
+        },
+        mounted() {
+            // 有儿子
+            // console.log(this.$children);
+            console.log("row mounted");
+            console.log(this.$children);
+            this.$children.forEach((vm) => {
+                vm.gutter = this.gutter;
+            })
+        }
     }
+
+    // var div = document.createElement('div')
+    // var childDiv = document.createElement('div')
+    // div.appendChild(childDiv)
+    // document.body.appendChild(div)
 </script>
 
 <style scoped lang="scss">
-.row{
-    display: flex;
-}
+    .row {
+        display: flex;
+    }
 </style>
