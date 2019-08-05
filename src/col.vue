746a5e9edc0612ab588d151dfbc28241f0c87fa9
chen <331862806@qq.com>
2019-08-05 10:58:47 +0800

    重构 row 和 col

--- src/col.vue
+++ src/col.vue
@@ -1,10 +1,6 @@
 <template>
-    <div class="col" :class="[span && `col-${span}`,offset && `offset-${offset}`]"
-         :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
-
-        <div style="border: 1px solid green; height: 100px;">
-            <slot></slot>
-        </div>
+    <div class="col" :class="colClass" :style="colStyle">
+        <slot></slot>
     </div>
 </template>
 
@@ -17,7 +13,7 @@
             },
             offset: {
                 type: [Number, String]
-            },
+            }
             // gutter: {
             //     type: [Number, String]
             // }
@@ -27,17 +23,24 @@
                 gutter: 0
             }
         },
-        created() {
-            console.log("row created")
-        },
-
-        mounted() {
-            console.log("row mounted");
-        },
+        computed: {
+            colClass() {
+                let {span, offset} = this;
+                return [
+                    span && `col-${span}`,
+                    offset && `offset-${offset}`
+                ]
+            },
+            colStyle() {
+                return {
+                    paddingLeft: this.gutter / 2 + 'px',
+                    paddingRight: this.gutter / 2 + 'px'
+                }
+            }
+        }
     }
 </script>
 <style scoped lang="scss">
-
     .col {
         width: 50%;
 
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
+    <div class="row" :style="rowStyle">
         <slot></slot>
     </div>
 </template>
@@ -12,26 +12,21 @@
                 type: [Number, String]
             }
         },
-        created() {
-            // 没有儿子，时间差
-            // console.log(this.$children);
-            console.log("row created")
+        computed: {
+            rowStyle() {
+                let {gutter} = this;
+                return {
+                    marginLeft: -gutter / 2 + 'px',
+                    marginRight: -gutter / 2 + 'px'
+                }
+            }
         },
         mounted() {
-            // 有儿子
-            // console.log(this.$children);
-            console.log("row mounted");
-            console.log(this.$children);
             this.$children.forEach((vm) => {
                 vm.gutter = this.gutter;
             })
         }
     }
-
-    // var div = document.createElement('div')
-    // var childDiv = document.createElement('div')
-    // div.appendChild(childDiv)
-    // document.body.appendChild(div)
 </script>
 
 <style scoped lang="scss">
