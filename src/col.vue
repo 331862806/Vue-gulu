fb2be3549f7b72268b834ed98a76a26df71cf807
chen <331862806@qq.com>
2019-08-08 14:20:35 +0800

    重构 col

--- src/col.vue
+++ src/col.vue
@@ -39,19 +39,34 @@
                 gutter: 0
             }
         },
+        methods: {
+            createClasses(obj, str = '') {
+                if (!obj) {
+                    return []
+                }
+                let array = [];
+                if (obj.span) {
+                    array.push(`col-${str}${obj.span}`)
+                }
+                if (obj.offset) {
+                    array.push(`offset-${str}${obj.offset}`)
+                }
+                return array;
+            }
+        },
         computed: {
             colClass() {
                 let {span, offset, ipad, narrowPc, pc, widePc} = this;
+                let createClasses = this.createClasses;
                 return [
-                    span && `col-${span}`,
-                    offset && `offset-${offset}`,
-                    // es6 拓展字符串
-                    ...(ipad ? [`col-ipad-${ipad.span}`] : []),
-                    ...(narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
-                    ...(pc ? [`col-pc-${pc.span}`] : []),
-                    ...(widePc ? [`col-wide-pc-${widePc.span}`] : []),
+                    ...createClasses({span, offset}),
+                    ...createClasses(ipad, 'ipad-'),
+                    ...createClasses(narrowPc, 'narrow-pc-'),
+                    ...createClasses(pc, 'pc-'),
+                    ...createClasses(widePc, 'wide-pc-')
                 ]
-            },
+            }
+            ,
             colStyle() {
                 return {
                     paddingLeft: this.gutter / 2 + 'px',
@@ -76,7 +91,7 @@
                 margin-left: ($n/24)*100%;
             }
         }
-        @media(min-width: 577px){
+        @media(min-width: 577px) {
             $class: col-ipad-;
             @for $n from 1 through 24 {
                 &.#{$class}#{$n} {
@@ -91,7 +106,7 @@
                 }
             }
         }
-        @media(min-width: 769px){
+        @media(min-width: 769px) {
             $class: col-narrow-pc-;
             @for $n from 1 through 24 {
                 &.#{$class}#{$n} {
@@ -106,7 +121,7 @@
                 }
             }
         }
-        @media(min-width: 993px){
+        @media(min-width: 993px) {
             $class: col-pc-;
             @for $n from 1 through 24 {
                 &.#{$class}#{$n} {
