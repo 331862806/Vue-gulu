85e3a57cad60695db16f537da115ad6463494de3
chen <331862806@qq.com>
2019-08-22 15:45:59 +0800

    优化 toast 的三种动画，并解决bug

--- src/toast.vue
+++ src/toast.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="toast" ref="wrapper" :class="toastClasses">
-        <div class="message">
-            <slot v-if="!enableHtml"></slot>
-            <div v-else v-html="$slots.default[0]"></div>
-        </div>
-        <div class="line" ref="line"></div>
-        <span class="close" v-if="closeButton" @click="onClickClose">
+    <div class="wrapper" :class="toastClasses">
+        <div class="toast" ref="toast">
+            <div class="message">
+                <slot v-if="!enableHtml"></slot>
+                <div v-else v-html="$slots.default[0]"></div>
+            </div>
+            <div class="line" ref="line"></div>
+            <span class="close" v-if="closeButton" @click="onClickClose">
             {{closeButton.text}}
         </span>
+        </div>
     </div>
 </template>
 
@@ -62,8 +64,8 @@
             updateStyles() {
                 //通过获取父亲的高度来设置自身的高度，使得有高度就可以100%
                 this.$nextTick(() => {
-                    // console.log(this.$refs.wrapper.getBoundingClientRect().height);
-                    this.$refs.line.style.height = `${this.$refs.wrapper.getBoundingClientRect().height}px`
+                    // console.log(this.$refs.toast.getBoundingClientRect().height);
+                    this.$refs.line.style.height = `${this.$refs.toast.getBoundingClientRect().height}px`
                 })
             },
             execAutoClose() {
@@ -97,25 +99,68 @@
     $font-size: 14px;
     $toast-min-height: 40px;
     $toast-bg: rgba(0, 0, 0, 0.75);
-    @keyframes fade_in {
+    $animation-duration: 300ms;
+    @keyframes slide-up {
         0% {opacity: 0; transform: translateY(100%);}
         100% {opacity: 1;transform: translateY(0%);}
     }
 
+    @keyframes slide-down {
+        0% {opacity: 0; transform: translateY(-100%);}
+        100% {opacity: 1;transform: translateY(0%);}
+    }
+
+    @keyframes fade-in {
+        0% {opacity: 0;}
+        100% {opacity: 1;}
+    }
+
+    .wrapper {
+        position: fixed;
+        left: 50%;
+        transform: translateX(-50%);
+
+        &.position-top {
+            top: 0;
+
+            .toast {
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+                animation: slide-down $animation-duration;
+            }
+        }
+
+        &.position-bottom {
+            bottom: 0;
+
+            .toast {
+                border-bottom-left-radius: 0;
+                border-bottom-right-radius: 0;
+                animation: slide-up $animation-duration;
+            }
+        }
+
+        &.position-middle {
+            top: 50%;
+            transform: translate(-50%, -50%);
+
+            .toast {
+                animation: fade-in $animation-duration;
+            }
+        }
+    }
+
     .toast {
-        animation: fade_in 1s;
         font-size: $font-size;
         min-height: $toast-min-height;
         line-height: 1.8;
         color: white;
-        position: fixed;
         display: flex;
         align-items: center;
         background: $toast-bg;
         border-radius: 4px;
         box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
         padding: 0 16px;
-        left: 50%;
 
         > .message {
             padding: 8px 0;
@@ -131,22 +176,6 @@
             height: 100%;
             border-left: 1px solid #666666;
             margin-left: 16px;
-        }
-
-        &.position-top {
-            top: 0;
-            transform: translateX(-50%);
-
-        }
-
-        &.position-bottom {
-            bottom: 0;
-            transform: translateX(-50%);
-        }
-
-        &.position-middle {
-            top: 50%;
-            transform: translate(-50%, -50%);
         }
     }
 
