71a2eb244d64d9030d0641ffc9b4539bfc93cdc6
chen <331862806@qq.com>
2019-09-18 16:04:44 +0800

    解决当容器div 有 overflow=hidden 时候的BUG

--- src/app.js
+++ src/app.js
@@ -49,9 +49,9 @@ new Vue({
 
     },
     methods: {
-        yyy(data){
+        yyy(){
             console.log('yyy');
-            console.log(data);
+            // console.log(data);
         },
         showToast1() {
             this.showToast('top')
--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="popover" @click.stop="xxx">
-        <div class="content-wrapper" v-if="visible" @click.stop>
+        <div ref="contentWrapper" class="content-wrapper" v-show="visible">
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 
@@ -16,15 +18,15 @@
         methods: {
             xxx() {
                 this.visible = !this.visible;
-                console.log('切换visible');
                 if (this.visible === true) {
                     this.$nextTick(() => {
-                        console.log("新增 document click 监听器");
+                        document.body.appendChild(this.$refs.contentWrapper)
+                        let { width, height, top, left } = this.$refs.triggerWrapper.getBoundingClientRect()
+                        console.log(width, height, top, left);
+                        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
                         let eventHandler = () => {
-                            console.log('点击body就关闭popover');
                             this.visible = false
-                            console.log("删除监听器");
-                            console.log("document 隐藏 popover");
                             document.removeEventListener('click', eventHandler)
                         }
                         document.addEventListener('click', eventHandler)
@@ -33,7 +35,7 @@
                     console.log("vm 隐藏 popover")
                 }
             }
-        }
+        },
     }
 </script>
 
@@ -42,14 +44,13 @@
         display: inline-block;
         vertical-align: top;
         position: relative;
+    }
 
-        .content-wrapper {
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            border: 1px solid red;
-            box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-        }
+    .content-wrapper {
+        position: absolute;
+        border: 1px solid red;
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        transform: translateY(-100%);
     }
 
 </style>
